8b3b995808d4aab0da237b59e7e84a99cc4b2067
RollyLif <relifungula@gmail.com>
2022-10-26 16:33:24 +0200

    feat: connection to database

--- index.js
+++ index.js
@@ -1,6 +1,24 @@
 import express from "express";
 import todosRouter from "./routes/todosRouter.js";
+import * as pg from "pg";
+
 const app = express();
+
+const {Client}=pg;
+const client = new Client({
+    host: "localhost",
+    user : "postgres",
+    port : 5432,
+    password :"",
+    database :"postgres"
+
+});
+
+client.connect();
+
+console.log("connexion réussie")
+
+
 app.use('/todos', todosRouter)
 app.listen(3001, () => {
     console.log('le serveur est lancé au port 3001')
