c2868446620330b87ba4b075169b40cb0cd91f8b
NdekoCode <arickbulakali@gmail.com>
2022-10-26 14:53:04 +0200

    update code

--- controllers/TodoController.js
+++ controllers/TodoController.js
@@ -1,8 +1,25 @@
+import { readFile } from "node:fs/promises";
+import { fileURLToPath } from "node:url";
+import path from "path";
+
+const todos = [
+  {
+    id: 25545353,
+    title: "Ma première tâche",
+    completed: false,
+  },
+];
 export default class TodoController {
-    createTodo(req, res) {
-
-    }
-    getAllTodos(req, res) {
-
-    }
+  createTodo(req, res) {
+    const todo = {
+      id: Date.now(),
+      title: req.body.title,
+      completed: false,
+    };
+    todos.push(todo);
+    return res.status(201).send(todo);
+  }
+  getAllTodos(req, res) {
+    return res.status(200).send(todos);
+  }
 }
--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 import express from "express";
 import todosRouter from "./routes/todosRouter.js";
 const app = express();
-app.use('/todos', todosRouter)
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use("/todos", todosRouter);
 app.listen(3001, () => {
-    console.log('le serveur est lancé au port 3001')
+  console.log("le serveur est lancé au port 3001");
 });
