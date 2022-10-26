24d84d28b03d4e0124c1836440e9b06e2006ddab
herilion <heritierlionge@yahoo.com>
2022-10-26 12:43:47 +0200

    code review

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
-const express= require("express");
+const express = require("express");
 const app = express();
 
-app.listen(3001, ()=>{
-    console.log('le serveur est a l ecoute au port 3001')
+app.listen(3001, () => {
+    console.log('le serveur est lancé au port 3001')
 });
