effae3810c9991f723b4f5a9bd0b82d2cb2f25dd
obedi bedjo <obedindevu@gmail.com>
2022-10-27 11:41:01 +0200

    fix: clear the * in delete req

--- controllers/TodoController.js
+++ controllers/TodoController.js
@@ -45,7 +45,7 @@ export default class TodoController {
   deleteTodo(req, res) 
   { 
   const id= parseInt(req.params.id)
-  const sql = `DELETE * FROM todos WHERE id=$1`
+  const sql = `DELETE FROM todos WHERE id=$1`
 
   try {
     (async (id) => {
